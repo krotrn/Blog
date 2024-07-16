d4096d78fea2f6c9ef6e7c93ee1255f2931ae2bd
krotrn <karankumarsahtkg2018@gmail.com>
2024-07-16 21:32:57 +0530

    Text Login Required and Text No Post Available

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
 import { Container, Loading, Postcard } from '../components'
 import appwriteService from '../appwrite/config'
-
+import { useSelector } from 'react-redux'
 
 
 function Home() {
     const [posts, setPosts] = React.useState([])
     const [loading, setLoading] = React.useState(true)
+    const authStatus = useSelector((state) => state.auth.status);
 
     useEffect(() => {
         appwriteService.getPosts().then((post) => {
@@ -24,23 +25,21 @@ function Home() {
     }
 
 
-    if (posts.length === 0) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full min-h-96 ">
-                            <h1 className="text-2xl font-bold hover:text-[#5f5691]">
-                                Login to read posts
-                            </h1>
-                        </div>
+    if (authStatus && posts.length === 0) {
+        return (<div className="w-full py-8 mt-4 text-center">
+            <Container>
+                <div className="flex flex-wrap">
+                    <div className="p-2 w-full min-h-96 ">
+                        <h1 className="text-2xl font-bold hover:text-[#5f5691]">
+                            No Post available
+                        </h1>
                     </div>
-                </Container>
-            </div>
-        )
+                </div>
+            </Container>
+        </div>)
     }
     return (
-        <div className='w-full py-8'>
+        authStatus ? (<div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
                     {posts.map((post) => (
@@ -54,7 +53,19 @@ function Home() {
                     ))}
                 </div>
             </Container>
-        </div>
+        </div>) : (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full min-h-96 ">
+                            <h1 className="text-2xl font-bold hover:text-[#5f5691]">
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
     )
 }
 
